e1c5fbd2e391876adf8710a5f62841d24e7ac188
Harvey Rogers <harveyr@gmail.com>
2019-11-13 23:02:12 -0800

    script tweak

--- scripts/push_release.sh
+++ scripts/push_release.sh
@@ -12,12 +12,13 @@ fi
 
 set -x
 
+npm ci
+npm run build
+git add -f lib
+git diff --quiet && git diff --staged --quiet || git commit -m "build"
+
 npm ci --only=production
 git add -f node_modules
 git diff --quiet && git diff --staged --quiet ||  git commit -m "node_modules"
-
-npm run build
-git add lib
-git diff --quiet && git diff --staged --quiet || git commit -am "build"
 
 git push
