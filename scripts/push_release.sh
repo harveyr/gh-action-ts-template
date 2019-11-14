45d6148f2b3fa18e0eae7c24812d9fa2587051bc
Harvey Rogers <harveyr@gmail.com>
2019-11-13 16:14:17 -0800

    push_release.sh

---
+++ scripts/push_release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Script to commit and push source and node_modules to a releases/ branch.
+
+set -e
+
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [[ ${BRANCH} != releases/* ]]; then
+  echo "This can run only on release branches. Current branch is ${BRANCH}"
+  exit 1
+fi
+
+set -x
+
+npm ci --only=production
+git add -f node_modules
+git diff --quiet && git diff --staged --quiet ||  git commit -m "node_modules"
+
+npm run build
+git add lib
+git diff --quiet && git diff --staged --quiet || git commit -am "build"
+
+git push
