fd4a1f9bf3e7d08f16289f3d5821bc95d964ac47
Harvey Rogers <harveyr@gmail.com>
2019-11-13 20:41:21 -0800

    prettier, catch error

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,21 @@
-import * as core from '@actions/core';
-import {wait} from './wait'
+import * as core from '@actions/core'
+import { wait } from './wait'
 
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
+    const ms = core.getInput('milliseconds')
     console.log(`Waiting ${ms} milliseconds ...`)
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms, 10));
-    core.debug((new Date()).toTimeString())
+    core.debug(new Date().toTimeString())
+    await wait(parseInt(ms, 10))
+    core.debug(new Date().toTimeString())
 
-    core.setOutput('time', new Date().toTimeString());
+    core.setOutput('time', new Date().toTimeString())
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
 }
 
-run();
+run().catch(err => {
+  core.setFailed(`${err}`)
+})
