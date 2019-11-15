25d012e3bdd9075eac899e61981f433d88391dc4
Harvey Rogers <harveyr@gmail.com>
2019-11-15 15:02:14 -0800

    tweak main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,15 @@
 import * as core from '@actions/core'
 import { wait } from './wait'
 
-async function run() {
-  try {
-    const ms = core.getInput('milliseconds')
-    console.log(`Waiting ${ms} milliseconds ...`)
+async function run(): Promise<void> {
+  const ms = core.getInput('milliseconds')
+  console.log(`Waiting ${ms} milliseconds ...`)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+  core.debug(new Date().toTimeString())
+  await wait(parseInt(ms, 10))
+  core.debug(new Date().toTimeString())
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+  core.setOutput('time', new Date().toTimeString())
 }
 
 run().catch(err => {
