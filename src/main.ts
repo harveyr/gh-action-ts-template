f57e49c69bde9dea0174d45a0d4a22d28549652e
Harvey Rogers <harveyr@gmail.com>
2019-11-15 15:14:51 -0800

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,7 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
 
 async function run(): Promise<void> {
-  const ms = core.getInput('milliseconds')
-  console.log(`Waiting ${ms} milliseconds ...`)
-
-  core.debug(new Date().toTimeString())
-  await wait(parseInt(ms, 10))
-  core.debug(new Date().toTimeString())
-
-  core.setOutput('time', new Date().toTimeString())
+  console.log('Hello cyber')
 }
 
 run().catch(err => {
